c0039855191b67bc1d3cb204e4d4f07aacc6393c
Diego Hernandez <diego.hernandez@conceptomovil.com>
2023-04-18 18:21:04 -0600

    Angular. Header

--- Angular/FrontAngular/src/app/app.component.ts
+++ Angular/FrontAngular/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
+@Component({  //Configuracion
+  selector: 'app-root', //Etiqueta html que sirve para cargar el componente en el HTML
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FrontAngular';
+  title = 'Bienvenido a Angular';
+  curso: string = 'Curso Udemy Spring con Angular';
+  nombre: string = 'Diego Hernández Cote';
 }
